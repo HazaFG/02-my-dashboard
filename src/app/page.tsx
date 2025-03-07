27ef276dafff4a82e1534a17bae04ebc386ada3a
Haza Pro <hazaelfg@gmail.com>
2025-03-07 13:54:47 -0700

    sidebar dinamico, memorizalo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 export default function HomePage() {
 
   //Esta pagina, la page.tsx principal basicamente es eso, la pagina principa, pero con esto podemos redirigir primero a este sitio, para eso el redirect, el cual hay que exportar
-  redirect('dashboard/counter')
+  redirect('dashboard/main')
 
   //Este codigo se pone mas oscuro, y es porque este codigo ya no retorna nada mas, o sea, esto nunca se va a usar xd, lo voy a comentar para que sirva de ejemplo
   // return (
